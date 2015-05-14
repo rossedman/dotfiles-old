5f2d880d91e172f8ab7733a87732d81cb54c2d96
Ross Edman <ross@axzm.com>
2015-05-13 23:40:43 -0500

    Setup

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+./init/.osx
+&& ./init/.fonts
+&& ./init/.casks
+&& ./init/.subl
+&& ./init/.atom
+&& ./init/.gituser
+&& ./init/.shell
+&& ./init/.vim
+&& ./init/.rvm
+&& ./init/.gems
+&& ./init/.composer
+&& ./init/.npm
+&& ./init/.homestead
+&& ./init/.symfony
