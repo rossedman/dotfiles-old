8837706dc06d52e40daebe8fa5a8d4eb5cf7a317
Ross Edman <ross.edman@gmail.com>
2015-05-19 23:53:30 -0500

    Setup Chruby

---
+++ ruby/chruby.zsh
@@ -0,0 +1,3 @@
+# Source Chruby
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
--- ruby/rvm.zsh
+++
@@ -1,5 +0,0 @@
-# Source RVM
-source ~/.rvm/scripts/rvm
-
-# Add to path
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
