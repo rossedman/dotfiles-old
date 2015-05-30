2a4579c1d2038b74d5cd85453fad81cfe52c4f8b
Ross Edman <ross.edman@gmail.com>
2015-05-30 13:25:24 -0500

    Awesome Docker Helpers

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,6 +1,23 @@
 $(boot2docker shellinit 2> /dev/null)
 
-alias dockstop='docker stop $(docker ps -a -q)'
-alias dockrma='docker rm $(docker ps -a -q)'
-alias dockrmi='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")'
-alias dockip='boot2docker ip 2> /dev/null'
+alias dip='boot2docker ip 2> /dev/null'
+alias dkd="docker run -d -P"
+alias dki="docker run -t -i -P"
+
+db() { docker build -t="$1" .; }
+drs() { docker stop $(docker ps -a -q);  }
+dri() { docker rmi $(docker images | grep "^<none>" | awk "{print $3}"); }
+drm() { docker rm $(docker ps -q -a); }
+dent() { docker exec -i -t $1 /bin/bash; }
+
+dclean() {
+  docker stop $(docker ps -a -q);
+  docker rm $(docker ps -q -a);
+  docker rmi $(docker images | grep "^<none>" | awk "{print $3}");  
+}
+
+dnuke() {
+  docker stop $(docker ps -a -q);
+  docker rm $(docker ps -q -a);
+  docker rmi $(docker images -q);
+}
